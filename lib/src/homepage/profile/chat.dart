f7fe3de685e30947eef2fab0587f9b929b8f0db7
luke <lukefang29@gmail.com>
2022-03-22 18:02:53 +0800

    add profile page

---
+++ lib/src/homepage/profile/chat.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class chat extends StatefulWidget {
+  const chat({Key? key}) : super(key: key);
+
+  @override
+  State<chat> createState() => _chatState();
+}
+
+class _chatState extends State<chat> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/src/homepage/profile/numberswidget.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+class NumbersWidget extends StatelessWidget {
+  const NumbersWidget({Key? key}) : super(key: key);
+  final value1 = 69;
+  final value2 = 69;
+  final value3 = 69;
+
+  @override
+  Widget build(BuildContext context) => Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          buildButton(text: 'Completed', value: value1),
+          buildDivider(),
+          buildButton(text: 'Incompleted', value: value2),
+          buildDivider(),
+          buildButton(text: 'Pending', value: value3),
+        ],
+      );
+  Widget buildDivider() => Container(
+        height: 24,
+        child: const VerticalDivider(
+          color: Colors.black,
+        ),
+      );
+  Widget buildButton({required String text, required int value}) =>
+      MaterialButton(
+        padding: const EdgeInsets.symmetric(vertical: 4),
+        onPressed: () {},
+        materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              '$value',
+              style: const TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 18,
+              ),
+            ),
+            const SizedBox(height: 2),
+            Text(
+              text,
+              style: TextStyle(fontSize: 16),
+            ),
+          ],
+        ),
+      );
+}
